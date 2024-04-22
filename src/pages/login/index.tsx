d9f6ece7dc55ee6f21c041df276a47517d0934ef
Verani M <veranimaherna21@gmail.com>
2024-04-22 17:04:21 +0700

    Create title and logo molecules

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,13 @@
+import ButtonLoginWithGoogle from "binar/components/atoms/ButtonLoginGoogle";
+import LogoDanUcapan from "binar/components/molecules/LogoDanUcapan";
+
+const login = () => {
+  return (
+    <>
+      {/* <ButtonLoginWithGoogle /> */}
+      <LogoDanUcapan title= "Selamat Dyahbsdg" wording= "shdbhsydbcfhdbhj" />
+    </>
+  );
+};
+
+export default login;
