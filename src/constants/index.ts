163c0b32a81aa8417d5747139acdc2fd9c57ad37
Sultan Rayhan <sultan.rayhan@binar.co.id>
2024-04-22 11:38:05 +0700

    add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+    primary: "#933393",
+    success: "#0EAF59",
+    error: "#DB2065",
+    disabled: "#C2C2C2",
+  },
+  fonts: {
+    heading: "Open Sans, sans-serif",
+    body: "Causten, sans-serif",
+  },
+} as const;
+
+export default theme;
